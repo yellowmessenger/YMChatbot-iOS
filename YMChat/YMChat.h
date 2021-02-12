fc3c3677286cf6935e8caba7b6b834b5e449b2c1
Kaunteya <k.suryawanshi@gmail.com>
2021-02-12 14:48:04 +0530

    Initial Commit

---
+++ YMChat/YMChat.h
@@ -0,0 +1,18 @@
+//
+//  YMChat.h
+//  YMChat
+//
+//  Created by Kauntey Suryawanshi on 12/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YMChat.
+FOUNDATION_EXPORT double YMChatVersionNumber;
+
+//! Project version string for YMChat.
+FOUNDATION_EXPORT const unsigned char YMChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YMChat/PublicHeader.h>
+
+
