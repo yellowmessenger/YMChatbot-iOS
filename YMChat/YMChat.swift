d6b7bdd51da15e6727925900b3c8068760475927
Kaunteya Suryawanshi <k.suryawanshi@gmail.com>
2021-08-10 12:36:43 +0530

    Fixed error message of customBaseURL

--- YMChat/YMChat.swift
+++ YMChat/YMChat.swift
@@ -22,7 +22,7 @@ import UIKit
             throw NSError(domain: "Bot id is not set. Please set botId before calling startChatbot()", code: 0, userInfo: nil)
         }
         if config.customBaseUrl.isEmpty {
-            throw NSError(domain: "CustomBaseURL is empty. Please set botId before calling startChatbot()", code: 0, userInfo: nil)
+            throw NSError(domain: "`customBaseURL` should not be empty.", code: 0, userInfo: nil)
         }
         try JSONSerialization.data(withJSONObject: config.payload, options: [])
     }
