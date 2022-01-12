507a5ba0a49cba0aec73044b0f013db1ea73c2af
Sankalp Gupta <sankalpgupta021193@gmail.com>
2022-01-13 00:22:43 +0530

    asset bundle updated

--- YMChat/Bundle.swift
+++ YMChat/Bundle.swift
@@ -13,6 +13,11 @@ extension Bundle {
         // Returns self bundle if installed using xcframework
         //
         // A separate bundle is required when user integrates statically using cocoapods
+        
+        #if SWIFT_PACKAGE
+            return Bundle.module
+        #endif
+        
         let moduleBundle = Bundle(for: YMChat.self)
         if let url = moduleBundle.url(forResource: "YMImages", withExtension: "bundle"),
            let imageBundle = Bundle(url: url) {
