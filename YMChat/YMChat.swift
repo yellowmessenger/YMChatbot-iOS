1d8cde8e6240df041c6b986bc89f23202b220f4b
Sankalp Gupta <sankalpgupta021193@gmail.com>
2022-09-12 13:20:32 +0530

    Public apis added

--- YMChat/YMChat.swift
+++ YMChat/YMChat.swift
@@ -64,55 +64,173 @@ public class YMChat: NSObject, YMChatViewControllerDelegate {
 
     @objc public func unlinkDeviceToken(botId: String, apiKey: String, deviceToken: String, success: @escaping () -> Void, failure: @escaping (String) -> Void) {
         precondition(!botId.isEmpty && !apiKey.isEmpty && !deviceToken.isEmpty)
-
-        let url = URL(string: "https://app.yellow.ai/api/plugin/removeDeviceToken")!
-        var urlComponent = URLComponents(url: url, resolvingAgainstBaseURL: false)!
-        urlComponent.queryItems = [URLQueryItem(name: "bot", value: botId)]
-
-        var request = URLRequest(url: urlComponent.url!)
-
-        request.httpMethod = "POST"
-        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
-        request.setValue(apiKey, forHTTPHeaderField: "x-api-key")
-
-
-        let body = ["deviceToken": deviceToken]
-        guard let bodyData = try? JSONSerialization.data(withJSONObject: body) else {
-            return
-        }
-        request.httpBody = bodyData
-
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if error != nil {
-                failure(error!.localizedDescription)
+        do {
+            try validateConfig()
+            
+            let url = URL(string: config.customBaseUrl + "/api/plugin/removeDeviceToken")!
+            var urlComponent = URLComponents(url: url, resolvingAgainstBaseURL: false)!
+            urlComponent.queryItems = [URLQueryItem(name: "bot", value: botId)]
+            
+            var request = URLRequest(url: urlComponent.url!)
+            
+            request.httpMethod = "POST"
+            request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+            request.setValue(apiKey, forHTTPHeaderField: "x-api-key")
+            
+            
+            let body = ["deviceToken": deviceToken]
+            guard let bodyData = try? JSONSerialization.data(withJSONObject: body) else {
                 return
             }
-            guard let httpResponse = response as? HTTPURLResponse, (200..<300).contains(httpResponse.statusCode) else {
-                let code = (response as? HTTPURLResponse)?.statusCode ?? -1
-                failure("Failed with status \(code)")
-                return
-            }
-            guard let data = data else {
-                failure("Response data empty")
-                return
-            }
-            guard let dict = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {
-                failure("Invalid response data")
-                return
-            }
-            if let isSuccess = dict["success"] as? Bool {
-                if isSuccess {
-                    success()
+            request.httpBody = bodyData
+            
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                if error != nil {
+                    failure(error!.localizedDescription)
                     return
                 }
-                let message = dict["message"] as? String ?? "Something went wrong"
-                failure(message)
-                return
-            }
-            failure("Something went wrong")
-        }.resume()
+                guard let httpResponse = response as? HTTPURLResponse, (200..<300).contains(httpResponse.statusCode) else {
+                    let code = (response as? HTTPURLResponse)?.statusCode ?? -1
+                    failure("Failed with status \(code)")
+                    return
+                }
+                guard let data = data else {
+                    failure("Response data empty")
+                    return
+                }
+                guard let dict = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {
+                    failure("Invalid response data")
+                    return
+                }
+                if let isSuccess = dict["success"] as? Bool {
+                    if isSuccess {
+                        success()
+                        return
+                    }
+                    let message = dict["message"] as? String ?? "Something went wrong"
+                    failure(message)
+                    return
+                }
+                failure("Something went wrong")
+            }.resume()
+        } catch {
+            failure(error.localizedDescription)
+        }
     }
 
+    @objc public func registerDevice(botId: String, apiKey: String, deviceToken: String, userId: String, success: @escaping () -> Void, failure: @escaping (String) -> Void) {
+        precondition(!botId.isEmpty && !apiKey.isEmpty && !deviceToken.isEmpty)
+
+        do {
+            try validateConfig()
+            let url = URL(string: config.customBaseUrl + "/api/mobile/register")!
+            var urlComponent = URLComponents(url: url, resolvingAgainstBaseURL: false)!
+            urlComponent.queryItems = [URLQueryItem(name: "bot", value: botId)]
+            
+            var request = URLRequest(url: urlComponent.url!)
+            
+            request.httpMethod = "POST"
+            request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+            request.setValue(apiKey, forHTTPHeaderField: "x-api-key")
+            
+            
+            let body = ["token": deviceToken,
+                        "userId": userId]
+            guard let bodyData = try? JSONSerialization.data(withJSONObject: body) else {
+                return
+            }
+            request.httpBody = bodyData
+            
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                if error != nil {
+                    failure(error!.localizedDescription)
+                    return
+                }
+                guard let httpResponse = response as? HTTPURLResponse, (200..<300).contains(httpResponse.statusCode) else {
+                    let code = (response as? HTTPURLResponse)?.statusCode ?? -1
+                    failure("Failed with status \(code)")
+                    return
+                }
+                guard let data = data else {
+                    failure("Response data empty")
+                    return
+                }
+                guard let dict = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {
+                    failure("Invalid response data")
+                    return
+                }
+                if let isSuccess = dict["success"] as? Bool {
+                    if isSuccess {
+                        success()
+                        return
+                    }
+                    let message = dict["message"] as? String ?? "Something went wrong"
+                    failure(message)
+                    return
+                }
+                failure("Something went wrong")
+            }.resume()
+        } catch {
+            failure(error.localizedDescription)
+        }
+    }
+
+    @objc public func getUnreadMessageCount(botId: String, apiKey: String, userId: String, success: @escaping (String) -> Void, failure: @escaping (String) -> Void) {
+        precondition(!botId.isEmpty && !apiKey.isEmpty && !userId.isEmpty)
+
+        do {
+            try validateConfig()
+            let url = URL(string: config.customBaseUrl + "/api/mobile/unreadMessages")!
+            var urlComponent = URLComponents(url: url, resolvingAgainstBaseURL: false)!
+            urlComponent.queryItems = [URLQueryItem(name: "bot", value: botId)]
+            
+            var request = URLRequest(url: urlComponent.url!)
+            
+            request.httpMethod = "POST"
+            request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+            request.setValue(apiKey, forHTTPHeaderField: "x-api-key")
+            
+            
+            let body = ["userId": userId]
+            guard let bodyData = try? JSONSerialization.data(withJSONObject: body) else {
+                return
+            }
+            request.httpBody = bodyData
+            
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                if error != nil {
+                    failure(error!.localizedDescription)
+                    return
+                }
+                guard let httpResponse = response as? HTTPURLResponse, (200..<300).contains(httpResponse.statusCode) else {
+                    let code = (response as? HTTPURLResponse)?.statusCode ?? -1
+                    failure("Failed with status \(code)")
+                    return
+                }
+                guard let data = data else {
+                    failure("Response data empty")
+                    return
+                }
+                guard let dict = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {
+                    failure("Invalid response data")
+                    return
+                }
+                if let isSuccess = dict["success"] as? Bool {
+                    if isSuccess, let data = dict["data"] as? [String: Any], let unreadCount = data["unreadCount"] as? String {
+                        success(unreadCount)
+                        return
+                    }
+                    let message = dict["message"] as? String ?? "Something went wrong"
+                    failure(message)
+                    return
+                }
+                failure("Something went wrong")
+            }.resume()
+        } catch {
+            failure(error.localizedDescription)
+        }
+    }
+    
     // MARK: - YMChatViewControllerDelegate
     func eventReceivedFromBot(code: String, data: String?) {
         if code == "bot-closed" {
