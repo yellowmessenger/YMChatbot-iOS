a9c0280e2f55d83d5c0db8a31fa648c82abf39aa
Sankalp Gupta <sankalpgupta021193@gmail.com>
2022-09-14 08:46:04 +0530

    Api signature updated

--- YMChat/YMChat.swift
+++ YMChat/YMChat.swift
@@ -35,6 +35,22 @@ public class YMChat: NSObject, YMChatViewControllerDelegate {
         try JSONSerialization.data(withJSONObject: config.payload, options: [])
     }
 
+    func validateConfig(config: YMConfig) throws {
+        if config.botId.isEmpty {
+            throw NSError(domain: "Bot id is not set. Please set botId before calling startChatbot()", code: 0, userInfo: nil)
+        }
+        if config.customBaseUrl.isEmpty {
+            throw NSError(domain: "`customBaseURL` should not be empty.", code: 0, userInfo: nil)
+        }
+        if config.customLoaderUrl.isEmpty || (URL(string: config.customLoaderUrl) == nil) {
+            throw NSError(domain: "Please provide valid `customLoaderUrl`", code: 0, userInfo: nil)
+        }
+        if !(config.version == 1 || config.version == 2) {
+            throw NSError(domain: "version can be either 1 or 2", code: 0, userInfo: nil)
+        }
+        try JSONSerialization.data(withJSONObject: config.payload, options: [])
+    }
+
     @discardableResult
     @objc public func initialiseView() throws -> YMChatViewController {
         try validateConfig()
@@ -62,14 +78,65 @@ public class YMChat: NSObject, YMChatViewControllerDelegate {
         })
     }
 
+    @available(*, deprecated, renamed: "unlinkDeviceToken(apiKey:ymConfig:success:failure:)")
     @objc public func unlinkDeviceToken(botId: String, apiKey: String, deviceToken: String, success: @escaping () -> Void, failure: @escaping (String) -> Void) {
         precondition(!botId.isEmpty && !apiKey.isEmpty && !deviceToken.isEmpty)
+        let url = URL(string: "https://app.yellow.ai/api/plugin/removeDeviceToken")!
+        var urlComponent = URLComponents(url: url, resolvingAgainstBaseURL: false)!
+        urlComponent.queryItems = [URLQueryItem(name: "bot", value: botId)]
+        
+        var request = URLRequest(url: urlComponent.url!)
+        
+        request.httpMethod = "POST"
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        request.setValue(apiKey, forHTTPHeaderField: "x-api-key")
+        
+        
+        let body = ["deviceToken": deviceToken]
+        guard let bodyData = try? JSONSerialization.data(withJSONObject: body) else {
+            return
+        }
+        request.httpBody = bodyData
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if error != nil {
+                failure(error!.localizedDescription)
+                return
+            }
+            guard let httpResponse = response as? HTTPURLResponse, (200..<300).contains(httpResponse.statusCode) else {
+                let code = (response as? HTTPURLResponse)?.statusCode ?? -1
+                failure("Failed with status \(code)")
+                return
+            }
+            guard let data = data else {
+                failure("Response data empty")
+                return
+            }
+            guard let dict = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {
+                failure("Invalid response data")
+                return
+            }
+            if let isSuccess = dict["success"] as? Bool {
+                if isSuccess {
+                    success()
+                    return
+                }
+                let message = dict["message"] as? String ?? "Something went wrong"
+                failure(message)
+                return
+            }
+            failure("Something went wrong")
+        }.resume()
+    }
+    
+    @objc public func unlinkDeviceToken(apiKey: String, ymConfig: YMConfig, success: @escaping () -> Void, failure: @escaping (String) -> Void) {
+        precondition(!ymConfig.botId.isEmpty && !apiKey.isEmpty && (ymConfig.deviceToken != nil && !ymConfig.deviceToken!.isEmpty))
         do {
-            try validateConfig()
+            try validateConfig(config: ymConfig)
             
-            let url = URL(string: config.customBaseUrl + "/api/plugin/removeDeviceToken")!
+            let url = URL(string: ymConfig.customBaseUrl + "/api/plugin/removeDeviceToken")!
             var urlComponent = URLComponents(url: url, resolvingAgainstBaseURL: false)!
-            urlComponent.queryItems = [URLQueryItem(name: "bot", value: botId)]
+            urlComponent.queryItems = [URLQueryItem(name: "bot", value: ymConfig.botId)]
             
             var request = URLRequest(url: urlComponent.url!)
             
@@ -78,7 +145,7 @@ public class YMChat: NSObject, YMChatViewControllerDelegate {
             request.setValue(apiKey, forHTTPHeaderField: "x-api-key")
             
             
-            let body = ["deviceToken": deviceToken]
+            let body = ["deviceToken": ymConfig.deviceToken]
             guard let bodyData = try? JSONSerialization.data(withJSONObject: body) else {
                 return
             }
@@ -117,15 +184,15 @@ public class YMChat: NSObject, YMChatViewControllerDelegate {
             failure(error.localizedDescription)
         }
     }
-
-    @objc public func registerDevice(botId: String, apiKey: String, deviceToken: String, userId: String, success: @escaping () -> Void, failure: @escaping (String) -> Void) {
-        precondition(!botId.isEmpty && !apiKey.isEmpty && !deviceToken.isEmpty)
-
+    
+    @objc public func registerDevice(apiKey: String, ymConfig: YMConfig, success: @escaping () -> Void, failure: @escaping (String) -> Void) {
+        precondition(!ymConfig.botId.isEmpty && !apiKey.isEmpty && (ymConfig.deviceToken != nil && !ymConfig.deviceToken!.isEmpty) )
+        
         do {
-            try validateConfig()
+            try validateConfig(config: ymConfig)
             let url = URL(string: config.customBaseUrl + "/api/mobile/register")!
             var urlComponent = URLComponents(url: url, resolvingAgainstBaseURL: false)!
-            urlComponent.queryItems = [URLQueryItem(name: "bot", value: botId)]
+            urlComponent.queryItems = [URLQueryItem(name: "bot", value: ymConfig.botId)]
             
             var request = URLRequest(url: urlComponent.url!)
             
@@ -134,8 +201,8 @@ public class YMChat: NSObject, YMChatViewControllerDelegate {
             request.setValue(apiKey, forHTTPHeaderField: "x-api-key")
             
             
-            let body = ["token": deviceToken,
-                        "userId": userId]
+            let body = ["token": ymConfig.deviceToken!,
+                        "userId": ymConfig.ymAuthenticationToken!]
             guard let bodyData = try? JSONSerialization.data(withJSONObject: body) else {
                 return
             }
@@ -175,14 +242,14 @@ public class YMChat: NSObject, YMChatViewControllerDelegate {
         }
     }
 
-    @objc public func getUnreadMessageCount(botId: String, apiKey: String, userId: String, success: @escaping (String) -> Void, failure: @escaping (String) -> Void) {
-        precondition(!botId.isEmpty && !apiKey.isEmpty && !userId.isEmpty)
+    @objc public func getUnreadMessageCount(apiKey: String, ymConfig: YMConfig, success: @escaping (String) -> Void, failure: @escaping (String) -> Void) {
+        precondition(!ymConfig.botId.isEmpty && !apiKey.isEmpty && (ymConfig.ymAuthenticationToken != nil && !ymConfig.ymAuthenticationToken!.isEmpty))
 
         do {
-            try validateConfig()
+            try validateConfig(config: ymConfig)
             let url = URL(string: config.customBaseUrl + "/api/mobile/unreadMessages")!
             var urlComponent = URLComponents(url: url, resolvingAgainstBaseURL: false)!
-            urlComponent.queryItems = [URLQueryItem(name: "bot", value: botId)]
+            urlComponent.queryItems = [URLQueryItem(name: "bot", value: ymConfig.botId)]
             
             var request = URLRequest(url: urlComponent.url!)
             
@@ -191,7 +258,7 @@ public class YMChat: NSObject, YMChatViewControllerDelegate {
             request.setValue(apiKey, forHTTPHeaderField: "x-api-key")
             
             
-            let body = ["userId": userId]
+            let body = ["userId": ymConfig.ymAuthenticationToken!]
             guard let bodyData = try? JSONSerialization.data(withJSONObject: body) else {
                 return
             }
